2363fd92320e27f4ed17c9f7cde66cda8780f190
Максим Матвиенко <fifermas@gmail.com>
2024-10-05 18:53:53 +0300

    Edit hello.pas

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  Println('Привет, мир!')
+  Println('Привет, мир!');
+  var lang := ReadString('Введите язык (русский, английский, испанский):');
+  if lang = 'русский' then
+    Println('Привет, мир!')
+  else if lang = 'английский' then
+    Println('Hello, World!')
+  else if lang = 'испанский' then
+    Println('¡Hola, Mundo!')
 end.
